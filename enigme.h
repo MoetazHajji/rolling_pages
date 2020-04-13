5551e12f92d2dbdc96a41f1d89aeed99c58b27c1
mohamedaliazouzi <61392012+mohamedaliazouzi@users.noreply.github.com>
2020-04-13 22:17:47 +0100

    Add files via upload

---
+++ enigme.h
@@ -0,0 +1,16 @@
+#ifndef ENIGMES_H_
+
+#define ENIGMES_H_
+
+
+void solution(SDL_Surface *ecran,int d);
+
+void enigmes(SDL_Surface *ecran,int d);
+
+void good(SDL_Surface *ecran);
+
+void reponse(SDL_Surface *ecran,int d);
+
+
+
+#endif
